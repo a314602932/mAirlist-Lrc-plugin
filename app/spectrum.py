860a430418f261fda8358d9e617f3a0e8cdbd704
Yang Liu <60437466+a314602932@users.noreply.github.com>
2025-08-30 11:01:17 +0800

    Create spectrum.py

---
+++ app/spectrum.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from PyQt5 import QtWidgets
+
+class SpectrumWidget(QtWidgets.QWidget):
+    """占位：后续接入音频数据"""
+    def __init__(self, parent=None):
+        super().__init__(parent)
